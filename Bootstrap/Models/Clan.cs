874a25ebac9890147e40b772be487053c550db94
Eric doman <doman96@hotmail.com>
2016-05-02 10:57:55 -0700

    Add Basic MVC 5 web app with basic database management features

---
+++ Bootstrap/Models/Clan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Bootstrap.Models
+{
+    public class Clan
+    {
+        public int ID { get; set; }
+        public string Gamertag { get; set; }
+        public string Company { get; set; } 
+    }
+
+    public class ClanDBContext : DbContext
+    {
+        public DbSet<Clan> Clans { get; set; }
+    }
+
+}
