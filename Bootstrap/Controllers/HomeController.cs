cae0b4510be3e333addc19579be0ad5181d2b3df
Eric doman <doman96@hotmail.com>
2016-05-03 11:56:00 -0700

    Create results basic results page which displays clans and their members

--- Bootstrap/Controllers/HomeController.cs
+++ Bootstrap/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Bootstrap.Models;
 
 namespace Bootstrap.Controllers
 {
@@ -10,6 +11,38 @@ namespace Bootstrap.Controllers
     {
         public ActionResult Index()
         {
+            //TO DO: Search Logic goes here
+
+            //Search by gamertag
+
+            //Print Txt from - form
+            //ViewBag.Message = gamertag;
+
+            return View(new ViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(ViewModel model)
+        {
+            System.Diagnostics.Debug.WriteLine("Called overloaded method INDEX - {0} - Headered by [HttpPost], [ValidateAntiForgeryToken]", model.Name);
+            return RedirectToAction("Results", model);
+            //return View();
+        }
+
+        public ActionResult Results(ViewModel model)
+        {
+            //System.Diagnostics.Debug.WriteLine("Called overloaded method INDEX - {0} - Headered by [HttpPost], [ValidateAntiForgeryToken]", model.Name);
+
+            //take companyname and loop through players 
+            List<string> gamertags = Quartermaster.Quartermaster.GetGamertagsForCompany(model.Name);
+            ViewBag.ProgList = gamertags;
+            ViewBag.Message = "Company: " + model.Name;
+
+            //foreach (string member in gamertags)
+            //{
+
+            //}
+
             return View();
         }
 
